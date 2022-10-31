ee53914d27bfb839f07ef10d6ddb1e84c4f32f1f
tanishchhillar12 <75465541+tanishchhillar12@users.noreply.github.com>
2022-10-31 21:16:41 +0530

    Update Fibonacci.cpp

--- c++/Fibonacci.cpp
+++ c++/Fibonacci.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int n;
     cout << "Enter the number of terms" << endl;
-    cin >> n;
+    cin >> n;//taking the input from the user
     int a = 0, b = 1, sum = 0;
 
     cout << a << endl
